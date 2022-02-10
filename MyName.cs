a8a22f0b2d075b743ca9f228d0046e7176e7c57a
minniunicorn <nknknkf92@gmail.com>
2022-02-10 22:02:31 +0300

    Добавьте файлы проекта.

---
+++ MyName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_3
+{
+    class MyName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MySum/MySum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySum
+{
+    class MySum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum");
+            Console.WriteLine(args.Length);
+            Console.WriteLine(Convert.ToInt32(args[0])+Convert.ToInt32(args[1]));
+            Console.ReadKey();
+        }
+    }
+}
